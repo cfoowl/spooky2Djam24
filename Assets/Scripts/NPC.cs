21ab7488d65418ab7aa8590f332d5e41f7bd6947
cfoowl <alicevancypher@gmail.com>
2024-10-26 17:38:26 +0200

    [Cfoowl] random movement

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    public float centerRadius = 2f;
     [SerializeField] private float speed = 1f;
     [HideInInspector] public bool canMove;
+    public Vector2 randomDestination = Vector2.zero;
+    private bool inCoroutine = false;
 
     void Start() {
         canMove = true;
+        StartCoroutine(SelectRandomDestination(new WaitForSeconds(0)));
     }
     void Update()
     {
@@ -21,12 +26,33 @@ public class NPC : MonoBehaviour
         Vector2 targetPosition = Vector2.zero; 
         Vector2 currentPosition = transform.position;
         
-        if (Vector2.Distance(currentPosition, targetPosition) > 0.1f)
+        if (Vector2.Distance(currentPosition, targetPosition) > centerRadius)
         {
-            Vector2 direction = (targetPosition - currentPosition).normalized; 
-
+            // Go to the center
             transform.position = Vector2.MoveTowards(currentPosition, targetPosition, speed * Time.deltaTime);
+        } else {
+            // Random movement
+            if(randomDestination != Vector2.zero) {
+                if (Vector2.Distance(currentPosition, randomDestination) > 0.1f) {
+                    transform.position = Vector2.MoveTowards(currentPosition, randomDestination, speed * Time.deltaTime);
+                } else {
+                    if (!inCoroutine) {
+                        StartCoroutine(SelectRandomDestination(new WaitForSeconds(Random.Range(2f,5f))));
+                    }
+                }
+            }
+            
         }
 
+    }
+
+    IEnumerator SelectRandomDestination(WaitForSeconds wait) {
+        inCoroutine = true;
+        float radius = centerRadius - 0.5f;
+        float targetX = Random.Range(-radius, radius);
+        float targetY = Random.Range(-radius, radius);
+        yield return wait;
+        randomDestination = new Vector2(targetX, targetY);
+        inCoroutine = false;
     }
 }
